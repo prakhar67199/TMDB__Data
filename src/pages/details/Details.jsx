140d84b82262526010f78b310c82cf6b7cea69d4
prakhar chandra gupta <prakharguptahr10@gmail.com>
2023-05-28 02:17:34 +0530

    file-corrections

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,7 +3,7 @@ import './style.scss';
 import { useParams } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch';
 import Cast from "./cast/Cast";
-import DetailsBanner from './detailsBanner/detailsBanner';
+import DetailsBanner from './detailsBanner/DetailsBanner';
 import VideosSection from './videoSection/VideoSection';
 import Similar from './carousels/similar';
 import Recommendation from './carousels/Recommendation';
